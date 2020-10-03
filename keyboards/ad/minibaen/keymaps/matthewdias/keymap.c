0bd77c45f8fd4c4be277d418480a177efa4509bb
matthewdias <matthewdias@me.com>
2020-10-02 21:04:41 -0500

    Add minibaen layout

---
+++ keyboards/ad/minibaen/keymaps/matthewdias/keymap.c
@@ -0,0 +1,26 @@
+#include QMK_KEYBOARD_H
+
+#define _QWERTY 0
+#define _FN 1
+#define _NUM 1
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_QWERTY] = LAYOUT(
+    KC_ESC,  KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,    _______, KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,    KC_BSPC,
+    KC_TAB,  KC_A,    KC_S,    KC_D,     KC_F,    KC_G,    KC_ENT,  KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,    _______, KC_N,    KC_M,    KC_COMMA, KC_DOT,  KC_SLSH, KC_RSFT,
+    KC_LCTL, KC_LALT, KC_LGUI, MO(_NUM), _______, _______, KC_SPC,  _______, MO(_FN), _______,  KC_RGUI, KC_RALT, KC_RCTL
+  ),
+  [_FN] = LAYOUT(
+    KC_GRV,  KC_MPRV, KC_MPLY, KC_MNXT, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_PGDN, KC_UP,   KC_LBRC, KC_RBRC, KC_TRNS,
+    KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MINS, KC_EQL,  KC_BSLS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  ),
+  [_NUM] = LAYOUT(
+    KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_TRNS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_4,    KC_5,    KC_6,    KC_TRNS, KC_TRNS,
+    KC_TRNS, RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_1,    KC_2,    KC_3,    KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_0,    KC_TRNS, KC_TRNS, KC_TRNS
+  ),
+};
