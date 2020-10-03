7e049b7c45d18df9b3b4f54d87205e09ba296d68
matthewdias <matthewdias@me.com>
2020-10-03 11:40:05 -0500

    Saner layout

--- keyboards/ad/minibaen/keymaps/matthewdias/keymap.c
+++ keyboards/ad/minibaen/keymaps/matthewdias/keymap.c
@@ -7,13 +7,13 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
     KC_ESC,  KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,    _______, KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,    KC_BSPC,
-    KC_TAB,  KC_A,    KC_S,    KC_D,     KC_F,    KC_G,    KC_ENT,  KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN, KC_QUOT,
+    KC_TAB,  KC_A,    KC_S,    KC_D,     KC_F,    KC_G,    KC_B,    KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN, KC_ENT,
     KC_RSFT, KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,    _______, KC_N,    KC_M,    KC_COMMA, KC_DOT,  KC_SLSH, KC_LSFT,
     KC_LCTL, KC_LALT, KC_LGUI, MO(_NUM), _______, _______, KC_SPC,  _______, MO(_FN), _______,  KC_RGUI, KC_RALT, KC_RCTL
   ),
   [_FN] = LAYOUT(
     KC_GRV,  KC_MPRV, KC_MPLY, KC_MNXT, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_PGDN, KC_UP,   KC_LBRC, KC_RBRC, KC_TRNS,
-    KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  KC_TRNS,
+    KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  KC_QUOT,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MINS, KC_EQL,  KC_BSLS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
